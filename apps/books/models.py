afd40e7991d50525b52802c48624ffa68c9a6bc9
riazuddinse <riaahm.se@gmail.com>
2025-02-10 00:26:07 +0500

    Add apps and health check endpoint

--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Book(models.Model):
+    id = models.UUIDField(primary_key=True, blank=False, default=uuid.uuid4)
+    title = models.CharField(blank=False, max_length=255)
+
+    def __str__(self):
+        return super().__str__(self.title)
--- apps/books/views.py
+++ apps/books/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
 
-@api_view(['GET'])
-def hello_world(request):
-    return Response({"message": "Hello, World!"})
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
+    # GET /api/books/
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
